aa9f8ac7452d67bf7074d33097b869f25a071829
Vyom Yadav <73882557+Vyom-Yadav@users.noreply.github.com>
2021-01-10 19:23:08 +0530

    some modifications

--- worldHunger/settings.py
+++ worldHunger/settings.py
@@ -23,9 +23,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = ')_)6mb6nwf8%$s8m3_k1sy&))p*+0$2h&zn)52sx5k0wnsjpie'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
-ALLOWED_HOSTS = ['127.0.0.1', 'worldhunger.herokuapp.com']
+ALLOWED_HOSTS = []
 
 
 # Application definition
@@ -43,7 +43,6 @@ INSTALLED_APPS = [
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
-    'whitenoise.middleware.WhiteNoiseMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
