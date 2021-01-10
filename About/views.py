6cb6321c021d0f7712d7379417bf3ab0f91e4612
Vyom Yadav <73882557+Vyom-Yadav@users.noreply.github.com>
2021-01-10 13:33:50 +0530

    Initial commit

---
+++ About/views.py
@@ -0,0 +1,11 @@
+from django.http import HttpResponse
+from django.shortcuts import render
+# Create your views here.
+
+
+def home_view(request):
+    return render(request, 'homepage.html')
+
+
+def About_Us(request):
+    return render(request, 'AboutUs.html')
