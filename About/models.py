eec67f11b24bcf11bdfaa1fca0a5f0f4290bbb3e
Vyom Yadav <73882557+Vyom-Yadav@users.noreply.github.com>
2021-01-13 22:09:35 +0530

    user rights

--- About/models.py
+++ About/models.py
@@ -1,3 +1,9 @@
 from django.db import models
 
-# Create your models here.
+
+class Warehouse(models.Model):
+    name = models.CharField(max_length=200)
+    weight = models.FloatField()
+    Time_duration = models.IntegerField()
+
+
